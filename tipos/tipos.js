e2c3a0509742b40a5a7cb2d624bd4efdd67b78f3
rodrigo.santos <ribeiro.santos@g.globo>
2020-01-24 17:51:42 -0300

    seguindo com tipos

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -25,3 +25,64 @@ console.log(typeof hobbies);
 hobbies = [100, 200, 300];
 //hobbies = 100 
 console.log(hobbies);
+//tuplas 
+var endereco = ["Av Principal", 99, ''];
+console.log(endereco);
+endereco = ["Rua Importante", 1260, "Bloco C"];
+console.log(endereco);
+// enums
+var Cor;
+(function (Cor) {
+    Cor[Cor["Cinza"] = 0] = "Cinza";
+    Cor[Cor["Verde"] = 100] = "Verde";
+    Cor[Cor["Azul"] = 10] = "Azul";
+    Cor[Cor["Laranja"] = 11] = "Laranja";
+    Cor[Cor["Amarelo"] = 12] = "Amarelo";
+    Cor[Cor["Vermelho"] = 100] = "Vermelho";
+})(Cor || (Cor = {}));
+var minhaCor = Cor.Verde;
+console.log(minhaCor);
+console.log(Cor.Azul);
+console.log(Cor.Laranja, Cor.Amarelo);
+console.log(Cor.Verde, Cor.Vermelho);
+// any 
+var carro = 'BMW';
+console.log(carro);
+carro = { marca: 'BMW', ano: 2019 };
+console.log(carro);
+//funcoes
+function retornaMeuNome() {
+    //return minhaIdade
+    return nome;
+}
+console.log(retornaMeuNome());
+function digaOi() {
+    console.log('Oi');
+}
+digaOi();
+function multiplicar(numA, numB) {
+    return numA * numB;
+}
+console.log(multiplicar(4.7, 9));
+//tipo funcao 
+var calculo;
+//calculo = digaOi 
+//calculo()
+calculo = multiplicar;
+//calculo = {}
+console.log(calculo(5, 6));
+//objetos 
+var usuario = {
+    nome: 'Joao',
+    idade: 27
+};
+console.log(usuario);
+/*usuario = {
+    name: 'Maria',
+    age: 31
+}*/
+usuario = {
+    nome: 'Maria',
+    idade: 31
+};
+console.log(usuario);
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -30,3 +30,83 @@ console.log(typeof hobbies)
 hobbies = [100, 200, 300]
 //hobbies = 100 
 console.log(hobbies)
+
+//tuplas 
+let endereco: [string, number, string] = ["Av Principal",99, '']
+console.log(endereco)
+
+
+endereco = ["Rua Importante",1260, "Bloco C"]
+console.log(endereco)
+
+// enums
+
+enum Cor{
+    Cinza, //0
+    Verde = 100, //100
+    Azul = 10, //10
+    Laranja, 
+    Amarelo, 
+    Vermelho = 100
+}
+
+let minhaCor: Cor = Cor.Verde
+console.log(minhaCor)
+
+console.log(Cor.Azul)
+console.log(Cor.Laranja, Cor.Amarelo)
+console.log(Cor.Verde, Cor.Vermelho)
+
+// any 
+let carro: any = 'BMW'
+console.log(carro)
+carro = {marca:'BMW', ano: 2019}
+console.log(carro)
+
+//funcoes
+function retornaMeuNome(): string{
+    //return minhaIdade
+    return nome  
+}
+
+console.log(retornaMeuNome())
+
+function digaOi(): void{
+    console.log('Oi')
+}
+
+digaOi() 
+
+function multiplicar(numA: number, numB: number): number{
+    return numA * numB 
+}
+console.log(multiplicar(4.7, 9))
+
+//tipo funcao 
+let calculo: (numeroA: number, numeroB: number) => number  
+//calculo = digaOi 
+//calculo()
+
+calculo = multiplicar
+//calculo = {}
+console.log(calculo(5,6))
+
+//objetos 
+let usuario: {nome: string, idade: number} = {
+    nome: 'Joao', 
+    idade: 27 
+}
+
+console.log(usuario)
+
+/*usuario = {
+    name: 'Maria', 
+    age: 31 
+}*/ 
+
+usuario = {
+    nome: 'Maria', 
+    idade: 31 
+}
+
+console.log(usuario)
